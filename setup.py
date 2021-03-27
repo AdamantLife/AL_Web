3984b63beaead59516b8421206a74784c296207f
AdamantLife <adamantlife@gmail.com>
2021-03-27 09:30:20 -0400

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="AL_Web",
+    version="1.0.0",
+    author="AdamantLife",
+    author_email="",
+    description="A collection of internet-usage code. Originally part of aclustoms",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AdamantLife/al_web",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    install_requires=[
+        "bs4",
+        "pillow",
+        "requests",
+
+        "git+https://github.com/AdamantLife/AL_Decorators",
+        ]
+)
